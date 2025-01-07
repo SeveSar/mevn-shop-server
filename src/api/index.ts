f014b1814c351adff0aab3d4a71b97b5872eca14
Artem <averin.dev@mail.ru>
2025-01-07 15:15:53 +0300

    fix: настройка дкплоя

--- src/api/index.ts
+++ src/api/index.ts
@@ -16,16 +16,27 @@ const app = express();
 const PORT = getEnv('PORT').required().asIntPositive();
 const whitelist = ["http://localhost:3000", "http://localhost:5050", 'https://mevn-shop-client-rho.vercel.app'];
 const corsOptions = {
-  credentials: true,
-  origin: function (origin: any, callback: any) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin || whitelist.includes(origin)) {
+      callback(null, true); // Разрешаем запросы с указанных доменов
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error("Not allowed by CORS")); // Отклоняем запрос
     }
   },
+  credentials: true, // Разрешаем отправку куков
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Разрешенные методы
+  allowedHeaders: [
+    "X-CSRF-Token",
+    "X-Requested-With",
+    "Accept",
+    "Accept-Version",
+    "Content-Length",
+    "Content-MD5",
+    "Content-Type",
+    "Date",
+    "X-Api-Version",
+  ],
 };
-
 app.use(express.static(path.join(__dirname, '../public')));
 // app.use(express.static(path.join(__dirname, 'images')));
 app.use(cors(corsOptions));
